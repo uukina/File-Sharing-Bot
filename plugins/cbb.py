a43f4a0c26f23cd3a82b6832075af1205a1b1f7e
uukdesu <54493113+uukina@users.noreply.github.com>
2021-12-29 15:29:58 +0700

    Revert "Update cbb.py"
    This reverts commit 44a8044f3bb000a96a6e0084914b9266e4d727aa.

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Hoster : <a href='tg://user?id={OWNER_ID}'>My Master</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a></b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>My Master</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
